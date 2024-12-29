28c780ac7eaa9ae21f26c6e41f23dc48d49b49fe
Luke Carl Thompson <lukecarlthompson@gmail.com>
2024-12-29 19:13:00 +0800

    simplify physics debugger

--- src/view/physics/physics-debugger.ts
+++ src/view/physics/physics-debugger.ts
@@ -1,36 +1,20 @@
-import { BufferAttribute, BufferGeometry, LineBasicMaterial, LineSegments } from "three";
+import { BufferAttribute, LineBasicMaterial, LineSegments } from "three";
 
 import type { World } from "@dimforge/rapier3d-compat";
 
-export type PhysicsDebuggerProps = {
-  physicsWorld: World;
-};
+export class PhysicsDebugger extends LineSegments {
+  public constructor() {
+    super();
+    this.material = new LineBasicMaterial({ vertexColors: true });
+  }
 
-export class PhysicsDebugger {
-  #physicsWorld: World;
-  #geometry = new BufferGeometry();
-  public readonly debugGraphics: LineSegments;
-
-  public constructor({ physicsWorld }: PhysicsDebuggerProps) {
-    const material = new LineBasicMaterial({ vertexColors: true });
-    this.#physicsWorld = physicsWorld;
+  public readonly update = (physicsWorld: World): void => {
     const buffers = physicsWorld.debugRender();
     const vertices = new BufferAttribute(buffers.vertices, 3);
     const colors = new BufferAttribute(buffers.colors, 4);
 
-    this.#geometry.setAttribute("position", vertices);
-    this.#geometry.setAttribute("color", colors);
-
-    this.debugGraphics = new LineSegments(this.#geometry, material);
-  }
-
-  public readonly update = (): void => {
-    const buffers = this.#physicsWorld.debugRender();
-    const vertices = new BufferAttribute(buffers.vertices, 3);
-    const colors = new BufferAttribute(buffers.colors, 4);
-
-    this.#geometry.setAttribute("position", vertices);
-    this.#geometry.setAttribute("color", colors);
-    this.#geometry.computeBoundingSphere();
+    this.geometry.setAttribute("position", vertices);
+    this.geometry.setAttribute("color", colors);
+    this.geometry.computeBoundingSphere();
   };
 }
