297409ad5cb4fc3b98aec75cea033786849ac5ed
Luke Carl Thompson <lukecarlthompson@gmail.com>
2024-12-30 21:15:59 +0800

    fix build error

--- src/view/components/example-component/add-controls.tweakpane.ts
+++ src/view/components/example-component/add-controls.tweakpane.ts
@@ -3,4 +3,7 @@ import type { Pane } from "tweakpane";
 
 export const addControls = (component: ExampleComponent, initialProps: ExampleComponent, pane: Pane): void => {
   // TODO: Add in some controls
+  void component;
+  void initialProps;
+  void pane;
 };
