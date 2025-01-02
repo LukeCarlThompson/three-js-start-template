c9cbf10ca684c87b5cd561806c9b5cbc7f1aebce
Luke Carl Thompson <lukecarlthompson@gmail.com>
2025-01-02 21:10:13 +0800

    simplify custom fog

--- src/view/custom-fog.ts
+++ src/view/custom-fog.ts
@@ -1,15 +1,13 @@
-import { color, fog, positionWorld, triNoise3D, uniform, vec3 } from "three/tsl";
+import { color, fog, positionWorld, time, triNoise3D, vec3 } from "three/tsl";
 
 import type { FogNode } from "three/webgpu";
 import type { ShaderNodeObject } from "three/tsl";
 
 export const createCustomFog = (colour: string): ShaderNodeObject<FogNode> => {
-  const timer = uniform(0).onFrameUpdate((frame) => frame.time);
-
   const multiplier = vec3(1, 1, 0.2);
 
-  const lowFrequencyNoise = triNoise3D(positionWorld.mul(multiplier).mul(0.008), 0.2, timer);
-  const highFrequencyNoise = triNoise3D(positionWorld.mul(multiplier).mul(0.01), 0.3, timer);
+  const lowFrequencyNoise = triNoise3D(positionWorld.mul(multiplier).mul(0.008), 0.2, time);
+  const highFrequencyNoise = triNoise3D(positionWorld.mul(multiplier).mul(0.01), 0.3, time);
   const combinedNoise = lowFrequencyNoise.add(highFrequencyNoise.mul(0.2)).mul(0.7);
 
   const fogColour = color(colour);
