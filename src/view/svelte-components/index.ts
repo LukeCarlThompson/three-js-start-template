e675a9acd564fe56635b2ec383cc29225899ab13
Luke Carl Thompson <lukecarlthompson@gmail.com>
2025-01-02 16:13:18 +0800

    add game over component

--- src/view/svelte-components/index.ts
+++ src/view/svelte-components/index.ts
@@ -1,3 +1,4 @@
+export { default as GameOver } from "./GameOver.svelte";
 export { default as GameOverlay } from "./GameOverlay.svelte";
 export { default as LevelComplete } from "./LevelComplete.svelte";
 export { default as LevelSelect } from "./LevelSelect.svelte";
