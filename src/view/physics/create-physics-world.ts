e9858c9062ad8a98df3208cb6b98662013e6c05f
Luke Carl Thompson <lukecarlthompson@gmail.com>
2025-01-03 10:47:06 +0800

    increase physics simulation accuracy

--- src/view/physics/create-physics-world.ts
+++ src/view/physics/create-physics-world.ts
@@ -4,11 +4,6 @@ export const createPhysicsWorld = async (): Promise<{ world: World; eventQueue: 
   const Rapier = await import("@dimforge/rapier3d-compat");
   await Rapier.init();
   const world = new Rapier.World({ x: 0, y: -30, z: 0 });
-
-  world.numSolverIterations = 1;
-  world.numAdditionalFrictionIterations = 1;
-  world.numInternalPgsIterations = 1;
-
   const eventQueue = new Rapier.EventQueue(true);
 
   return { world, eventQueue };
